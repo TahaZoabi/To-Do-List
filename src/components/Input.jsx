88ccb37d51c137a2e4b61326ebcb6ca897869f8a
Taha Zoabi <zoabi03@gmail.com>
2024-09-23 14:58:13 +0300

    useRef, form and label

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,35 +1,42 @@
-import { useContext, useState } from "react";
+import { useContext, useRef } from "react";
 import { GlobalContext } from "../Context/GlobalContext.jsx";
 
 function Input() {
-  const [todo, setTodo] = useState("");
+  const todo = useRef("");
   const { addTodo } = useContext(GlobalContext);
 
   function handleChange(e) {
-    setTodo(e.target.value);
+    todo.current = e.target.value;
   }
 
-  function handleClick() {
-    addTodo(todo);
-    setTodo("");
+  function handleClick(e) {
+    e.preventDefault();
+    if (todo.current.trim()) {
+      addTodo(todo.current);
+      todo.current = "";
+      e.target.elements[0].value = "";
+    }
   }
-
   return (
-    <div className="flex justify-center gap-2  items-center border-black-2 rounded">
+    <form
+      onSubmit={handleClick}
+      className="flex justify-center gap-2  items-center border-black-2 rounded"
+    >
+      <label htmlFor="todo-input" className="text-sm">
+        Add Todo:
+      </label>
       <input
-        value={todo}
+        id="todo-input"
+        ref={todo}
         onChange={handleChange}
         type="text"
         placeholder="add todo..."
         className="flex h-10 w-[400px] rounded-lg border border-input bg-white px-3 py-2 text-sm file:border-0  file:text-sm    focus-visible:outline-none focus-visible:ring-2 "
       />
-      <button
-        onClick={() => handleClick()}
-        className="bg-amber-100 py-2 px-4 rounded-lg border-none hover:bg-amber-300"
-      >
+      <button className="bg-amber-100 py-2 px-4 rounded-lg border-none hover:bg-amber-300">
         Add
       </button>
-    </div>
+    </form>
   );
 }
 
